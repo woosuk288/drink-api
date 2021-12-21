9006121ccb35d5a5b2de97a6e4e343a215fbfa18
양우석 <woosuk288@naver.com>
2021-12-21 23:59:29 +0900

    Initialize project using Nestjs with firebase

---
+++ src/main.ts
@@ -0,0 +1,36 @@
+import { NestFactory } from '@nestjs/core';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import 'firebase-functions';
+import { AppModule } from './app.module';
+import * as firebaseAdmin from 'firebase-admin';
+import * as functions from 'firebase-functions';
+import * as express from 'express';
+
+const server = express();
+
+export const createNestServer = async (expressInstance) => {
+  const app = await NestFactory.create(
+    AppModule,
+    new ExpressAdapter(expressInstance),
+    { logger: process.env.NODE_ENV === 'dev' ? ['log'] : ['error'] },
+  );
+
+  // Initialize the firebase admin app
+  firebaseAdmin.initializeApp();
+
+  app.enableCors();
+  // return app.init();
+  return process.env.NODE_ENV === 'dev'
+    ? app.listen(process.env.PORT || 4010)
+    : app.init();
+};
+
+createNestServer(server)
+  .then(() => console.log('Nest Ready : env is ', process.env.NODE_ENV))
+  .catch((error) => console.error('Nest broken', error));
+
+export const api = functions.region('asia-northeast3').https.onRequest(server);
+
+// TODO: better-firebase-functions
+// export * from './firebase/trggers';
+// export * from './firebase/schedules';
